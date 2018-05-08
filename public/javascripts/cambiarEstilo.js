0c2ef267cfa758107133b4c281a800a4235ebfe9
tfelder <tomas.felder@gmail.com>
2018-05-08 14:48:45 -0300

    Cambiar estilo sin loguearse prueba 2

--- public/javascripts/cambiarEstilo.js
+++ public/javascripts/cambiarEstilo.js
@@ -11,22 +11,27 @@ function CambiarEstilo(){
   var styleSheet = document.getElementById('estilo2');
   if (styleSheet.disabled== true){
     $.get('./api/guardarEstilo', { id: 1 }, function(data) {
+      console.log(data);
       if(data=='no'){
         localStorage.setItem("estilo","true");
         $("#estilo2").prop('disabled', false);
       }
-      localStorage.clear();
-      location.reload(true);
+      else{
+        localStorage.clear();
+        location.reload(true);
+      }
     });
   }
   else {
     $.get('./api/guardarEstilo', { id: 0 }, function(data) {
+      console.log(data);
       if(data=='no'){
         localStorage.setItem("estilo","false");
         $("#estilo2").prop('disabled', true);
+      }else{
+        localStorage.clear();
+        location.reload(true);
       }
-      localStorage.clear();
-      location.reload(true);
     });
   }
 
