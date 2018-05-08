34c4986df5d871f593de6acdcee01dd322df8ffd
tfelder <tomas.felder@gmail.com>
2018-05-08 14:40:36 -0300

    Cambio de estilo para no logueados

--- public/javascripts/cambiarEstilo.js
+++ public/javascripts/cambiarEstilo.js
@@ -1,15 +1,31 @@
-function CambiarEstilo(){
+$(document).ready(function(){
+  if(localStorage.getItem("estilo")=="true"){
+    $("#estilo2").prop('disabled', false);
+  }
+  if(localStorage.getItem("estilo")=="false"){
+    $("#estilo2").prop('disabled', true);
+  }
+}
 
+function CambiarEstilo(){
   var styleSheet = document.getElementById('estilo2');
   if (styleSheet.disabled== true){
     $.get('./api/guardarEstilo', { id: 1 }, function(data) {
-      console.log(data);
+      if(data=='no'){
+        localStorage.setItem("estilo","true");
+        $("#estilo2").prop('disabled', false);
+      }
+      localStorage.clear();
       location.reload(true);
     });
   }
   else {
     $.get('./api/guardarEstilo', { id: 0 }, function(data) {
-      console.log(data);
+      if(data=='no'){
+        localStorage.setItem("estilo","false");
+        $("#estilo2").prop('disabled', true);
+      }
+      localStorage.clear();
       location.reload(true);
     });
   }
