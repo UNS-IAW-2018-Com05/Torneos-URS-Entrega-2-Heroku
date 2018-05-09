c3266e8bd9acc50f4358c9130d39c6d53a1ccb39
tfelder <tomas.felder@gmail.com>
2018-05-08 23:24:00 -0300

    Estilo 5

--- public/javascripts/cambiarEstilo.js
+++ public/javascripts/cambiarEstilo.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   if(localStorage.getItem("estilo")=="true"){
     $("#estilo2").prop('disabled', false);
+    $("body").attr('background','');
   }
   if(localStorage.getItem("estilo")=="false"){
     $("#estilo2").prop('disabled', true);
+    $("body").attr('background','images/backgroundNigth.jpg');
   }
 
 });
@@ -17,6 +19,7 @@ function CambiarEstilo(){
       if(data=='no'){
         localStorage.setItem("estilo","true");
         $("#estilo2").prop('disabled', false);
+        $("body").attr('background','');
       }
       else{
         localStorage.clear();
@@ -31,6 +34,7 @@ function CambiarEstilo(){
       if(data=='no'){
         localStorage.setItem("estilo","false");
         $("#estilo2").prop('disabled', true);
+        $("body").attr('background','images/backgroundNigth.jpg');
       }else{
         localStorage.clear();
         location.reload(true);
