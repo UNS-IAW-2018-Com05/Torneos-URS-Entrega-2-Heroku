587ee6bb95159b76aa3a993bd37754885bfe058f
tfelder <tomas.felder@gmail.com>
2018-05-10 10:48:25 -0300

    Arreglo fechas y clubes

--- public/javascripts/cambioFechas.js
+++ public/javascripts/cambioFechas.js
@@ -12,14 +12,28 @@ function mostrarPartidos(id){
     url: './api/fecha',
     data: "id="+id,
     success: function(data) {
-        var partidos = data.partidos;
+        var partidos = data;
         var i;
+        var index;
         for(i=0;i<partidos.length;i++){
-            var card = document.getElementById("partido-"+(i+1)).childNodes;
-            var cardBody = card[3].childNodes;
-            cardBody[1].innerHTML = partidos[i].local.nombre+" - "+partidos[i].visitante.nombre;
-            cardBody[3].innerHTML = partidos[i].puntosLocal+" - "+partidos[i].puntosVisitante;
-            cardBody[5].childNodes[0].innerHTML = partidos[i].estado;
+          var date = new Date(partidos[i].dia);
+          year = date.getFullYear();
+          month = date.getMonth()+1;
+          dt = date.getDate();
+
+          if (dt < 10) {
+            dt = '0' + dt;
+          }
+          if (month < 10) {
+            month = '0' + month;
+          }
+          const fecha = dt+'-' + month + '-'+year;
+
+            document.getElementById("titulo-"+(i+1)).innerHTML =  partidos[i].local.nombre+" - "+partidos[i].visitante.nombre;
+            document.getElementById("puntos-"+(i+1)).innerHTML =  partidos[i].puntosLocal+" - "+partidos[i].puntosVisitante;
+            document.getElementById("dia-"+(i+1)).innerHTML ="<strong>Dia </strong>"+fecha;
+            document.getElementById("hora-"+(i+1)).innerHTML = "<strong>Hora </strong>"+ partidos[i].hora;
+            document.getElementById("estado-"+(i+1)).innerHTML =  "<small>"+partidos[i].estado+"</small>";
         }
 
     }
