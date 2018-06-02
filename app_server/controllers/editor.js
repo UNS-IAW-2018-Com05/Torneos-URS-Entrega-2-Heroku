be24b0fd3f8ec374d10b8cbf0c2f8046436061a5
tfelder <tomas.felder@gmail.com>
2018-06-02 17:35:15 -0300

    Partidos para editor solo para el editor elegido

--- app_server/controllers/editor.js
+++ app_server/controllers/editor.js
@@ -5,7 +5,7 @@ var user;
 
 const edicion = function (req, res) {
   user = req.user;
-  var query = Partido.find({'estado':'no iniciado'})
+  var query = Partido.find({'estado':'no iniciado','editor':req.user.googleid})
   .populate( [{ path: 'local', select: 'nombre'},{ path: 'visitante', select: 'nombre'}]);
   query.exec(function(err,partidos){
     if(err)
