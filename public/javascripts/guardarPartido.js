15ef81f55a82165a470631f35f5fa60292ee49a0
tfelder <tomas.felder@gmail.com>
2018-06-14 14:26:02 -0300

    Sacados los alerts

--- public/javascripts/guardarPartido.js
+++ public/javascripts/guardarPartido.js
@@ -4,18 +4,15 @@ function guardarPartido(boton) {
   if(puntoLocal.length>0 && puntoVisitante.length>0){
     const idPartido = boton.id;
     var dataPartido={id: idPartido, puntosLocal: puntoLocal , puntosVisitante: puntoVisitante, estado: 'finalizado'};
-    if (confirm('Datos correctos?')) {
       $.post('./api/partidoNuevo',
           dataPartido,
           function(data, status){
-              alert("Partido: " + data + " guardado.");
               $(boton).attr("disabled", true);
               var icon = $(boton).children()[0];
               $(icon).addClass('fas fa-check').removeClass('far fa-save');
               $(boton).css('background-color', 'rgb(89, 210, 105)');
               $(boton).css('cursor','pointer');
           });
-    }
   }
   else{
     alert("Ha ingresado valores nulos");
